ee42f472c3964863b6887166c5f5a6464b26da87
Maria <mariaguydefontgalland@gmail.com>
2024-01-21 09:42:39 +0100

    (feat:) formatage des dates des épreuves

--- components/DisplayEpreuveItem.js
+++ components/DisplayEpreuveItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from "moment";
 import { StyleSheet, View, Text, SafeAreaView, FlatList } from 'react-native';
 
 function DisplayEpreuveItem({epreuve}) {
@@ -6,13 +7,14 @@ function DisplayEpreuveItem({epreuve}) {
   console.log('-----------------------');
   console.log(epreuve);
   console.log('-----------------------');
+  console.log(moment(epreuve.debut_epreuve).utc().format('DD-MM-YYYY'));
   return (
       <View style={styles.containerEpreuves}>
         <Text style={styles.titreContainer}>Prochaine épreuve</Text>
         <View>
-          <Text style={styles.textesContainer} >{epreuve.nom_epreuve}</Text>
-          <Text style={styles.textesContainer} >{epreuve.debut_epreuve} - {epreuve.fin_epreuve}</Text>
-          <Text style={styles.textesContainer} >Lieu : {epreuve.SitesCompetitions.nom__sitecompetition}, {epreuve.SitesCompetitions.ville_sitecompetition}</Text>
+          <Text style={styles.textesContainer}>{epreuve.nom_epreuve}</Text>
+          <Text style={styles.textesContainer}>{moment(epreuve.debut_epreuve).utc().format('HH:mm DD-MM-YYYY')} - {moment(epreuve.fin_epreuve).utc().format('HH:mm DD-MM-YYYY')}</Text>
+          <Text style={styles.textesContainer}>Lieu : {epreuve.SitesCompetitions.nom__sitecompetition}, {epreuve.SitesCompetitions.ville_sitecompetition}</Text>
         </View>
         <Text  style={styles.btnVoirEpreuves}>Voir tous les épreuves</Text>
       </View>
