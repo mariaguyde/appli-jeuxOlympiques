0cd87ab840eaa1aa406087a0ae43f428c3ff0fa0
Maria <mariaguydefontgalland@gmail.com>
2024-01-23 20:45:17 +0100

    (feat:) affichage des markers sur la carte dynamiquement

--- components/Map.js
+++ components/Map.js
@@ -2,50 +2,25 @@ import React from 'react'
 import { Text, View, StyleSheet, FlatList, Dimensions } from 'react-native';
 import { useEffect, useState } from "react";
 import MapView, { Marker, Polyline, Callout } from 'react-native-maps';
+
+// todo sortir ça et le mettre à un seul endroit
 import { createClient } from '@supabase/supabase-js'; 
 const supabase = createClient("https://aqccddyuxofytfphhgoi.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxY2NkZHl1eG9meXRmcGhoZ29pIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMDIyNDI2MiwiZXhwIjoyMDE1ODAwMjYyfQ.4Gw-vzPfrcyW2kSBl54ObqHdN5_pIPASpnAczf4MLK4");
 
 
 function Map({navigation}) {
 
-
     const [coordonneesSiteCompetitions, setCoordonneesSiteCompetitions] = useState([]);
     useEffect(() => {
         getCoordonnesSiteCompetitions();
-        console.log(coordonneesSiteCompetitions);
     }, []);
-  
+
     async function getCoordonnesSiteCompetitions() {
         const { data, error } = await supabase
         .from('SitesCompetitions')
         .select('*');
         setCoordonneesSiteCompetitions(data);
-
-        coordonneesSiteCompetitions.forEach(element => {
-            console.log('------------- DEBUT    -------------');
-            element["cooordonnesRegroupes"] = {
-                latitude : element.positionlatitude__sitecompetition,
-                longitude : element.positionlongitude_sitecompetition
-            }
-
-            console.log(element);
-            console.log('-------------- FINl ------------');
-        });
-
     }
-
-    
-    const [coordonneesSiteCompetitions2] = useState([
-        {
-            latitude: 48.8587741,
-            longitude: 2.2069771,
-        },
-        {
-            latitude: 48.8323785,
-            longitude: 2.3361663,
-        },
-    ]);
-    //*/
 
     return (
         <View style={styles.container}>
@@ -57,20 +32,18 @@ function Map({navigation}) {
                 latitudeDelta: 0.0622,
                 longitudeDelta: 0.0121,
             }}>
-
-                <Marker coordinate={coordonneesSiteCompetitions2[0]} />
-                <Marker coordinate={coordonneesSiteCompetitions2[1]} />
-
-                
-                <FlatList
-                    data={coordonneesSiteCompetitions}
-                    renderItem={({item}) => <Marker coordinate={item.cooordonnesRegroupes} />}
-                    keyExtractor={(item) => {
-                      return item.id_sitecompetition;
-                    }}
-                />
+                 {
+                    coordonneesSiteCompetitions.map((item, index) => 
+                        <Marker
+                            key={index} 
+                            coordinate={{
+                                latitude: item.positionlatitude__sitecompetition,
+                                longitude: item.positionlongitude_sitecompetition
+                            }}
+                        />
+                    )
+                 }
             </MapView>
-
         </View>
     );
 }
