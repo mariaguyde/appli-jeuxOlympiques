6eb9d56e4d1a2943e981f20206961dd8ef9e59ee
Maria <mariaguydefontgalland@gmail.com>
2024-01-23 21:37:22 +0100

    (feat:) connexion supabase bdd déplace dans un fichier

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { StyleSheet, View, Text, SafeAreaView, FlatList, Button } from 'react-native';
 import { useEffect, useState } from "react";
-//import databaseConfig from './databaseConfig'; 
 import DisplayEpreuveItem from './DisplayEpreuveItem';
 import { createClient } from '@supabase/supabase-js'; 
-const supabase = createClient("https://aqccddyuxofytfphhgoi.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxY2NkZHl1eG9meXRmcGhoZ29pIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMDIyNDI2MiwiZXhwIjoyMDE1ODAwMjYyfQ.4Gw-vzPfrcyW2kSBl54ObqHdN5_pIPASpnAczf4MLK4");
+import { supabase } from '../lib/supabase'
 
 function Home({navigation}) {
     const [epreuves, setEpreuves] = useState([]);
--- components/Map.js
+++ components/Map.js
@@ -2,10 +2,7 @@ import React from 'react'
 import { Text, View, StyleSheet, FlatList, Dimensions } from 'react-native';
 import { useEffect, useState } from "react";
 import MapView, { Marker, Polyline, Callout } from 'react-native-maps';
-
-// todo sortir ça et le mettre à un seul endroit
-import { createClient } from '@supabase/supabase-js'; 
-const supabase = createClient("https://aqccddyuxofytfphhgoi.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxY2NkZHl1eG9meXRmcGhoZ29pIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMDIyNDI2MiwiZXhwIjoyMDE1ODAwMjYyfQ.4Gw-vzPfrcyW2kSBl54ObqHdN5_pIPASpnAczf4MLK4");
+import { supabase } from '../lib/supabase'
 
 
 function Map({navigation}) {
