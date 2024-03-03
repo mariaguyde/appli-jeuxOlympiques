c54328220a1c74f4531a8628df0cfc8dd0fb39db
Maria <mariaguydefontgalland@gmail.com>
2024-03-03 20:23:00 +0100

    (refactor:) structuration data en cours nativia

--- components/Home.js
+++ components/Home.js
@@ -5,7 +5,6 @@ import React from 'react'
 import { supabase } from '../lib/supabase'
 import { colors } from '../lib/constants'
 import DisplayEpreuveItem from './DisplayEpreuveItem';
-import axios from 'axios';
 
 function Home({navigation}) {
     const [epreuves, setEpreuves] = useState([]);
@@ -21,73 +20,6 @@ function Home({navigation}) {
       .limit(1);
       setEpreuves(data);
     }
-
-    const fetchDataNativia = async () => {
-      console.log('fetchData NATIVIA');
-        try {
-            // TODO à dynamiser 
-            const response = await axios.get(
-              "https://api.navitia.io/v1/journeys?from=2.36066%3B48.84237&to=2.35475%3B48.88035&"
-            , {
-              headers: {
-                  'Authorization': `69aa33e5-d8c5-4b2c-b571-63383a3915c7`,
-              }
-            });
-            console.log(response.data.journeys[0].sections.length);
-
-            let itineraire = [];
-            console.log("______________________ " + Date.now() + " __________________________");
-
-            console.log("On va faire le chemin suivant : "); 
-            for (let i=0; i<response.data.journeys[0].sections.length; i++) {
-                //console.log(response.data.journeys[0].sections[i]);
-                let boutItineraire = response.data.journeys[0].sections[i]; 
-                itineraire[i] = {
-                  mode: boutItineraire.mode, 
-                  directionsLiterraire: boutItineraire.path, 
-                  pointDepart: boutItineraire.from, 
-                  pointArrivee: boutItineraire.to, 
-                  departureTime: response.data.journeys[0].departure_date_time,
-                  arriveeTime: response.data.journeys[0].arrival_date_time
-                }
-
-                console.log("- " + itineraire[i].pointDepart.name + " - " + itineraire[i].pointArrivee.name );
-                //console.log("- " + convertDate(itineraire[i].pointDepart.departure_date_time) + itineraire[i].pointDepart.name + " - " + itineraire[i].pointArrivee.name );
-                //console.log(convertDate(itineraire[i].arriveeTime));
-                //console.log(convertDate(itineraire[i].arriveeTime));
-
-            }
-            //console.log(response.data.journeys[0].arrival_date_time);
-            console.log("________________________________________________");
-
-        } catch (err) {
-            console.log(err.response);
-        }
-    }
-
-    const convertDate= (chaineDateHeure) => {
-
-      // Extraire les composants de la chaîne
-      var annee = parseInt(chaineDateHeure.substr(0, 4), 10);
-      var mois = parseInt(chaineDateHeure.substr(4, 2), 10) - 1; // Mois est 0-indexé dans JavaScript, donc on soustrait 1
-      var jour = parseInt(chaineDateHeure.substr(6, 2), 10);
-      var heure = parseInt(chaineDateHeure.substr(9, 2), 10);
-      var minute = parseInt(chaineDateHeure.substr(11, 2), 10);
-      var seconde = parseInt(chaineDateHeure.substr(13, 2), 10);
-
-      // Créer un objet Date
-      var date = new Date(annee, mois, jour, heure, minute, seconde);
-
-      // Formater la date
-      //var options = { hour: 'numeric', minute: 'numeric', day: '2-digit', month: '2-digit', year: 'numeric' };
-      //var dateFormatee = date.toLocaleString('fr-FR', options);
-
-      var dateFormatee = date.toLocaleString('fr-FR', options);
-      // Obtenir uniquement la partie de l'heure
-      var heureFormatee = date.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
-
-      return heureFormatee; 
-  }
 
 
     fetchDataNativia();
--- components/Map.js
+++ components/Map.js
@@ -5,19 +5,19 @@ import MapView, { Marker, Polyline, Callout } from 'react-native-maps';
 import { supabase } from '../lib/supabase'
 import { PROVIDER_GOOGLE } from 'react-native-maps';
 import Modal from "react-native-modal";
+import axios from 'axios';
 
 
 function Map({navigation}) {
 
-    const [coordonneesSiteCompetitions, setCoordonneesSiteCompetitions] = useState([]);
-    useEffect(() => {
-        getCoordonnesSiteCompetitions();
-    }, []);
-
-
     const [isModalVisible, setIsModalVisible] = useState(false);
     const handleModal = () => setIsModalVisible(() => !isModalVisible);
-  
+    const [coordonneesSiteCompetitions, setCoordonneesSiteCompetitions] = useState([]);
+    const [itineraireEtapes, setItineraireEtapes] = useState([]);
+    useEffect(() => {
+        getCoordonnesSiteCompetitions();
+        fetchDataNativia();
+    }, []);
 
     async function getCoordonnesSiteCompetitions() {
         const { data, error } = await supabase
@@ -30,11 +30,74 @@ function Map({navigation}) {
         setIsModalVisible(true);
     }
 
+    async function fetchDataNativia () {
+        console.log('fetchData NATIVIA');
+          try {
+              // TODO à dynamiser 
+              const response = await axios.get(
+                "https://api.navitia.io/v1/journeys?from=2.36066%3B48.84237&to=2.35475%3B48.88035&"
+              , {
+                headers: {
+                    'Authorization': `69aa33e5-d8c5-4b2c-b571-63383a3915c7`,
+                }
+              });
+  
+              let itineraire = [];
+              console.log("______________________ " + Date.now() + " __________________________");
+  
+              console.log("On va faire le chemin suivant : "); 
+              for (let i=0; i<response.data.journeys[0].sections.length; i++) {
+                  //console.log(response.data.journeys[0].sections[i]);
+                  let boutItineraire = response.data.journeys[0].sections[i]; 
+                  itineraire[i] = {
+                    mode: boutItineraire.mode, 
+                    directionsLiterraire: boutItineraire.path, 
+                    pointDepart: boutItineraire.from, 
+                    pointArrivee: boutItineraire.to, 
+                    departureTime: response.data.journeys[0].departure_date_time,
+                    arriveeTime: response.data.journeys[0].arrival_date_time
+                  }
+  
+                  console.log("- " + itineraire[i].pointDepart.name + " - " + itineraire[i].pointArrivee.name );
+                  //console.log("- " + convertDate(itineraire[i].pointDepart.departure_date_time) + itineraire[i].pointDepart.name + " - " + itineraire[i].pointArrivee.name );
+              }
+              console.log("________________________________________________");
+              setItineraireEtapes(itineraire);
+
+              console.log('itineraire pho');
+              console.log(itineraireEtapes);
+          } catch (err) {
+              console.log(err.response);
+          }
+    }
+  
+    const convertDate = (chaineDateHeure) => {
+  
+        // Extraire les composants de la chaîne
+        var annee = parseInt(chaineDateHeure.substr(0, 4), 10);
+        var mois = parseInt(chaineDateHeure.substr(4, 2), 10) - 1; // Mois est 0-indexé dans JavaScript, donc on soustrait 1
+        var jour = parseInt(chaineDateHeure.substr(6, 2), 10);
+        var heure = parseInt(chaineDateHeure.substr(9, 2), 10);
+        var minute = parseInt(chaineDateHeure.substr(11, 2), 10);
+        var seconde = parseInt(chaineDateHeure.substr(13, 2), 10);
+  
+        // Créer un objet Date
+        var date = new Date(annee, mois, jour, heure, minute, seconde);
+  
+        // Formater la date
+        //var options = { hour: 'numeric', minute: 'numeric', day: '2-digit', month: '2-digit', year: 'numeric' };
+        //var dateFormatee = date.toLocaleString('fr-FR', options);
+  
+        var dateFormatee = date.toLocaleString('fr-FR', options);
+        // Obtenir uniquement la partie de l'heure
+        var heureFormatee = date.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
+  
+        return heureFormatee; 
+    }
+
 
     // Chercher la position du tel  // @react-native-get-location
     // Faire l'itineraire entre la position du tel et le site de compet (avec Navidia)
-    
-    // provider = {PROVIDER_GOOGLE}
     return (
         <View style={styles.container}>
             <MapView
@@ -60,7 +123,10 @@ function Map({navigation}) {
                  }
             </MapView>
             <Modal style={styles.modalItineraire} isVisible={isModalVisible}>
-                  <Text>Am i displaying something for you ? </Text>
+                  
+                  <Text>Test Modal</Text>
+                  {/*<Text>{itineraireEtapes[0].mode}</Text>{//*/}
+
                   <Button title="Hide modal" onPress={handleModal} />
             </Modal>
         </View>
